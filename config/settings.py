cbe1ea9f7993fbed5035ba9b4873965e98dfedd4
Akmal Melibaev <akmal.melibaev@gmail.com>
2021-02-27 12:09:25 +0500

    homepage created

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1', '0.0.0.0']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
 
-# Create your tests here.
+from core.views import HomePageView
+
+
+class HomePageTests(SimpleTestCase):
+    def setUp(self) -> None:
+        self.response = self.client.get(reverse('home'))
+
+    def test_homepage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_homepage_template(self):
+        self.assertTemplateUsed(self.response, 'home.html')
+
+    def test_homepage_contains_correct_html(self):
+        self.assertContains(self.response, 'Direct Pro Homepage')
+
+    def test_homepage_does_not_contain_incorrect_html(self):
+        self.assertNotContains(self.response, 'I should not be on the page!')
+
+    def test_homepage_url_resolves_homepageview(self):
+        view = resolve('/')
+        self.assertEqual(
+            view.func.__name__,
+            HomePageView.as_view().__name__
+        )
